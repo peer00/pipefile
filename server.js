4d6defe90856bd94dca5b8b19719a83fae0a00a4
Joel Konijn <joelkonijn@gmail.com>
2016-02-22 14:25:49 +0100

    second commit

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.get('/SpectorSoft_Product_Comparison_corp_Matrix.pdf', function (req, res) {
   request('https://nessys-my.sharepoint.com/personal/joel_nessys_nl/_layouts/15/guestaccess.aspx?guestaccesstoken=k3GtQBAn9W39iJxICXfX1tW4jB0u6SlDxRhAbdPI2ew%3d&docid=0082d17dd5bbf49c9b8e4c73da98f7b91').pipe(res);
 });
 
+app.get('/test.zip', function (req, res) {
+  request('https://nessys-my.sharepoint.com/personal/joel_nessys_nl/_layouts/15/guestaccess.aspx?guestaccesstoken=qAjeo%2fgc15oe1fK42s0cIbnc7z2HFRnoOYbxDBlnSQA%3d&docid=00042863a443f44a1a94a7743303b0ad1&expiration=2016%2f02%2f23+13%3a25%3a04').pipe(res);
+});
+
 app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
 });
